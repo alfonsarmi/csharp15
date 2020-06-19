ed2245782fa913b766e6f613a5f5e8ebf99fd53e
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2020-06-19 14:26:07 +0200

    first

---
+++ ConsoleApp4/ConsoleApp4/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Class1
+    {
+        public int testc  { get; set; }
+
+        public void Caca1() 
+        { 
+        }
+
+        public void Caca1(string a)
+        {
+        }
+
+        public void Caca2()
+        {
+        }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Class2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Class2 
+    {
+        
+        private void MethodGeniar()
+        {
+            Class1 invokacior = new Class1();
+
+            invokacior.Caca1();
+            invokacior.Caca2();
+            invokacior.testc = 1;
+            invokacior.Caca1("a");
+        }
+    }
+}
